3a2db05fc3440784322ffb8b059c969e22e6325f
caioaamaral <caioaamaral@gmail.com>
2025-03-04 12:30:05 -0300

    feat(states): send chat_history when asking prompts to gpt

--- mental_health/states/chat.py
+++ mental_health/states/chat.py
@@ -6,6 +6,13 @@ import reflex as rx
 from mental_health.services.gpt_client import GptClient
 
 
+def _stripe_chat_history(chat_history):
+    return '\n'.join(
+        f'Q: {question}\nA: {answer}\n'
+        for question, answer in chat_history
+    )
+
+
 class State(rx.State):
     # The current question being asked.
     question: str
@@ -21,7 +28,7 @@ class State(rx.State):
         answer = ''
         self.chat_history.append((self.question, answer))
 
-        async for answer in client.answer(self.question):
+        async for answer in client.answer(_stripe_chat_history(self.chat_history)):
             if self.question:
                 self.question = ''
                 yield
