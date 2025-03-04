f7181212a57dbefb1e5c438209c8383bf0789a3c
Caio Amaral <caioaamaral@gmail.com>
2025-03-04 19:24:32 -0300

    feat: insert question/answer separately (#7)
    * feat: force users to specify table when using DatabaseClient
    
    * feat: remove field promt_id from Message model schema
    
    * feat: save question/answer separately

--- mental_health/services/db_client.py
+++ mental_health/services/db_client.py
@@ -10,35 +10,41 @@ dotenv.load_dotenv()
 
 class _SupaBase:
 
-    def __init__(self, url: str, key: str, table_name: str):
+    def __init__(self, url: str, key: str):
         self._client = create_client(url, key)
-        self._table = self._client.table(table_name)
+        self._table = None
 
     def table(self, table_name: str):
         self._table = self._client.table(table_name)
 
     def insert(self, data: dict):
+        if self._table is None:
+            raise ValueError('No table selected.')
+
         self._table.insert(data).execute()
 
 
 class _MongoDB:
 
-    def __init__(self, url: str, table_name: str, database_name: str = 'mental_health'):
-        self._client = MongoClient(url, uuidRepresentation="standard")
-        self._collection = self._client[database_name][table_name]
+    def __init__(self, url: str, database_name: str = 'mental_health'):
+        self._client = MongoClient(url, uuidRepresentation="standard")[database_name]
+        self._collection = None
 
     def table(self, table_name: str):
         self._collection = self._client[table_name]
 
     def insert(self, model: BaseModel):
+        if self._collection is None:
+            raise ValueError('No collection selected.')
+
         self._collection.insert_one(model.model_dump())
 
 
 class DatabaseClient:
 
     MODELS = {
-        'supabase': lambda: _SupaBase(url=os.environ['SUPABASE_URL'], key=os.environ['SUPABASE_KEY'], table_name='mental_health'),
-        'mongodb': lambda: _MongoDB(url=os.environ['MONGO_URL'], table_name='mental_health')
+        'supabase': lambda: _SupaBase(url=os.environ['SUPABASE_URL'], key=os.environ['SUPABASE_KEY']),
+        'mongodb': lambda: _MongoDB(url=os.environ['MONGO_URL'])
     }
 
     def __init__(self, model: str):
@@ -52,4 +58,5 @@ class DatabaseClient:
         return self
 
     def insert(self, data: dict):
-        return self._client.insert(data)
+        self._client.insert(data)
+        return self
