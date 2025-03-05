e417c851ddc038c058bd4efbf7cd6253f681ae33
Daniel <danielrsenna@gmail.com>
2025-03-05 16:43:36 -0300

    feat: add scroll layout to chat and change message box colors

--- mental_health/styles/chatpage.py
+++ mental_health/styles/chatpage.py
@@ -5,7 +5,7 @@ shadow = "rgba(0, 0, 0, 0.15) 0px 2px 8px"
 chat_margin = "5%"
 message_style = dict(
     padding="1em",
-    border_radius="5px",
+    border_radius="8px",
     margin_y="0.5em",
     #box_shadow=shadow,
     max_width="30em",
@@ -15,11 +15,11 @@ message_style = dict(
 # Set specific styles for questions and answers.
 question_style = message_style | dict(
     margin_left=chat_margin,
-    background_color=rx.color("grass", 2),
+    background_color=rx.color("gray", 4),
 )
 answer_style = message_style | dict(
     margin_right=chat_margin,
-    background_color=rx.color("grass", 1),
+    background_color=rx.color("gray", 2),
 )
 
 # Styles for the action bar.
