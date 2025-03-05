ce3e49f1f43e262ee6b4ac8f4be686eec3550635
Daniel <danielrsenna@gmail.com>
2025-03-05 12:29:13 -0300

    refactor: alter hero section

--- mental_health/pages/homepage.py
+++ mental_health/pages/homepage.py
@@ -1,14 +1,78 @@
 import reflex as rx
 
-from ..ui import navbar, footer, hero_section
+from ..ui import navbar, footer
 
 def homepage() -> rx.Component:
     # Welcome Page (Index)
     return rx.flex(
         navbar(),
         hero_section(),
-        footer(),
+        #footer(),
         direction="column",
+        justify="center",
         align="center",
         background_color="#ffffff",
+        height="100vh",
+    )
+
+def hero_section() -> rx.Component:
+    """Render the hero section of the landing page."""
+    return rx.flex(
+        rx.text(
+            "Saúde mental acessível para todos.",
+            text_align="left",
+            #background_image="linear-gradient(95deg, #D6D6ED 42.14%, #727280 63.21%)",
+            font_size="40px",
+            #background_clip="text",
+            font_weight="bold",
+            line_height="1",
+            font_family= "Garamond",
+            #background_color="#000000",
+            color="#000000",  
+            width="50%",
+        ),
+        # rx.text(
+        #     "Kuatan fará isso realidade. É como ajudaremos você em seu caminho.",
+        #     text_align="left",
+        #     color="#000000", 
+        #     #background_color="#000000",
+        #     font_size="30px",
+        #     font_family= "Garamond",
+        #     font_weight="bold",
+        #     line_height="1",
+        #     #max_width=["300px", "350px", "650px", "650px", "650px", "650px"],
+        # ),
+        rx.text(
+            "Kuatan Saúde: Uma plataforma para pacientes, para psicólogos, para você.",
+            text_align="left",
+            color="#000000", 
+            #background_color="#000000",
+            font_size="30px",
+            font_family= "Garamond",
+            font_weight="bold",
+            line_height="1",
+            #max_width=["300px", "350px", "650px", "650px", "650px", "650px"],
+        ),
+        rx.link(
+            rx.button(
+                "Conhecer",
+                font_size="15px",
+                #font_family= "Garamond",
+                size="3",
+                weight="bold",
+                variant="solid",
+                radius="full",
+                background_color=rx.color("gray", 12),
+                high_contrast=False,
+            ),
+            href="/chatpage",
+        ),
+        spacing="2",
+        direction="column",
+        #background_color="red",
+        height="100vh",
+        width="100vw",
+        justify="center",
+        align="start",
+        padding="5em",
     )
--- mental_health/ui/__init__.py
+++ mental_health/ui/__init__.py
@@ -1,11 +1,9 @@
 from .footer import footer
 from .navbar import navbar 
-from .hero import hero_section 
 from .sidebar import sidebar_chat 
 
 __all__ = [
     'footer',
     'navbar',
-    'hero_section',
     'sidebar_chat',
 ]
